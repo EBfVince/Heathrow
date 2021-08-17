0657c8832e96b17f442c648649a393f022932b20
Vince <vincent.guillebaud31@gmail.com>
2021-08-17 23:44:21 +0200

    🚀 Change for Heroku deploy 2

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ async function bootstrap(): Promise<void> {
   const server = await createServer()
 
   server.listen({ port: process.env.PORT || 4000 }).then(async ({ url }) => {
-    console.log(`🚀 Server ready at: ${url}`)
+    console.log(`
+      🚀 Server ready at: ${url}
+      📭  Query at https://studio.apollographql.com/dev
+    `)
   })
 }
 
